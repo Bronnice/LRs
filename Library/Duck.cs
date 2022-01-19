9046558e25e40c304e35d8e5820b622f8c5fde45
kpc <kostroma.kpc@gmal.com>
2022-01-19 13:39:22 +0300

    Сделаны интерфейсы

--- Library/Duck.cs
+++ Library/Duck.cs
@@ -8,14 +8,11 @@ namespace Library
 {
     abstract public class Duck
     {
-        public string Quack()
-        {
-            return "Quack" + " " + this.GetType();
-        }
         public string Swim()
         {
             return "Swim" + " " + this.GetType();
         }
+
         public abstract string Display();
     }
 }
--- Library/RedheadDuck.cs
+++ Library/RedheadDuck.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    public class MallardDuck : Duck, Flyable, Quackable
+    public class RedheadDuck : Duck, Flyable, Quackable
     {
         public override string Display()
         {
-            return "Display" + " " + this.GetType();
+            return "Display 12" + " " + this.GetType();
         }
 
         public string Fly()
         {
             return "Fly" + " " + this.GetType();
         }
-        
+
         public string Quack()
         {
             return "Quack" + " " + this.GetType();
